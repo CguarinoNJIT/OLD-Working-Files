59c7efdf12816af13b78661eb55aaa9f55342bee
Chris Guarino <chris.francis.guarino@gmail.com>
2021-07-01 14:55:28 -0400

    Need to rework test functions. Data list is indistrimiately growing.

--- Projects/P1/src/CalculatorTests.py
+++ Projects/P1/src/CalculatorTests.py
@@ -18,36 +18,42 @@ class MyTestCase(unittest.TestCase):
 
 # Addition Check
     def test_add_method_calculator(self):
+        print("Addition: ")
         test_data = CsvReader('/src/Unit Test Addition.csv').data
         for row in test_data:
             self.assertEqual(self.calculator.add(int(row['Value 1']),int(row['Value 2'])), int(row['Result']))
             self.assertEqual(self.calculator.result, int(row['Result']))
 # Subtraction Check
     def test_subtract_method_calculator(self):
+        print("Subtraction: ")
         test_data = CsvReader('/src/Unit Test Subtraction.csv').data
         for row in test_data:
-            self.assertEqual(self.calculator.subtract(int(row['Value 2']),int(row['Value 1'])), int(row['Result']))
+            self.assertEqual(self.calculator.subtract(int(row['Value 1']),int(row['Value 2'])), int(row['Result']))
             self.assertEqual(self.calculator.result, int(row['Result']))
 # Multiplication Check
     def test_multiply_method_calculator(self):
+        print("Multiplication: ")
         test_data = CsvReader('/src/Unit Test Multiplication.csv').data
         for row in test_data:
             self.assertEqual(self.calculator.multiply(int(row['Value 1']),int(row['Value 2'])), int(row['Result']))
             self.assertEqual(self.calculator.result, int(row['Result']))
 # Division Check
     def test_division_method_calculator(self):
+        print("Division: ")
         test_data = CsvReader('/src/Unit Test Division.csv').data
         for row in test_data:
             self.assertEqual(self.calculator.divide(int(row['Value 1']),int(row['Value 2'])), int(row['Result']))
             self.assertEqual(self.calculator.result, int(row['Result']))
 # Square Root Check
     def test_squareroot_method_calculator(self):
+        print("Square Root: ")
         test_data = CsvReader('/src/Unit Test Square Root.csv').data
         for row in test_data:
             self.assertEqual(self.calculator.square_root(int(row['Value 1'])), int(row['Result']))
             self.assertEqual(self.calculator.result, int(row['Result']))
 # Square Check
     def test_squared_method_calculator(self):
+        print("Squared: ")
         test_data = CsvReader('/src/Unit Test Square.csv').data
         for row in test_data:
             self.assertEqual(self.calculator.squared(int(row['Value 1'])), int(row['Result']))
