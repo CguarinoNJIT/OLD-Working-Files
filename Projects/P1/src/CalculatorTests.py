b9354bf7c5adba647c3b813eeb127dfa9d3daf18
Chris Guarino <chris.francis.guarino@gmail.com>
2021-07-01 11:59:27 -0400

    PathError: Fixed! .csv needed more specific path names

--- Projects/P1/src/CalculatorTests.py
+++ Projects/P1/src/CalculatorTests.py
@@ -8,45 +8,47 @@ class MyTestCase(unittest.TestCase):
 # This setUp method will give scope of it's contents to the test methods that follow.
     def setUp(self) -> None:
         self.calculator = Calculator()
+
 # This test_instantiate_calculator confirms that a Calculator Class exists.
     def test_instantiate_calculator(self):
         self.assertIsInstance(self.calculator, Calculator)
 
     def test_results_property_calculator(self):
         self.assertEqual(self.calculator.result, 0)
+
 # Addition Check
     def test_add_method_calculator(self):
-        test_data = CsvReader('Unit Test Addition.csv').data
+        test_data = CsvReader('/src/Unit Test Addition.csv').data
         for row in test_data:
             self.assertEqual(self.calculator.add(int(row['Value 1']),int(row['Value 2'])), int(row['Result']))
             self.assertEqual(self.calculator.result, int(row['Result']))
 # Subtraction Check
     def test_subtract_method_calculator(self):
-        test_data = CsvReader('Unit Test Subtraction.csv').data
+        test_data = CsvReader('/src/Unit Test Subtraction.csv').data
         for row in test_data:
             self.assertEqual(self.calculator.subtract(int(row['Value 2']),int(row['Value 1'])), int(row['Result']))
             self.assertEqual(self.calculator.result, int(row['Result']))
 # Multiplication Check
     def test_multiply_method_calculator(self):
-        test_data = CsvReader('Unit Test Multiplication.csv').data
+        test_data = CsvReader('/src/Unit Test Multiplication.csv').data
         for row in test_data:
             self.assertEqual(self.calculator.multiply(int(row['Value 1']),int(row['Value 2'])), int(row['Result']))
             self.assertEqual(self.calculator.result, int(row['Result']))
 # Division Check
     def test_division_method_calculator(self):
-        test_data = CsvReader('Unit Test Division.csv').data
+        test_data = CsvReader('/src/Unit Test Division.csv').data
         for row in test_data:
             self.assertEqual(self.calculator.divide(int(row['Value 1']),int(row['Value 2'])), int(row['Result']))
             self.assertEqual(self.calculator.result, int(row['Result']))
 # Square Root Check
     def test_squareroot_method_calculator(self):
-        test_data = CsvReader('Unit Test Square Root.csv').data
+        test_data = CsvReader('/src/Unit Test Square Root.csv').data
         for row in test_data:
             self.assertEqual(self.calculator.square_root(int(row['Value 1'])), int(row['Result']))
             self.assertEqual(self.calculator.result, int(row['Result']))
 # Square Check
     def test_squared_method_calculator(self):
-        test_data = CsvReader('Unit Test Square.csv').data
+        test_data = CsvReader('/src/Unit Test Square.csv').data
         for row in test_data:
             self.assertEqual(self.calculator.squared(int(row['Value 1'])), int(row['Result']))
             self.assertEqual(self.calculator.result, int(row['Result']))
