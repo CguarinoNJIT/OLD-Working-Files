ee001bed9c429331f0e02893f3537bc614889e6e
CguarinoNJIT <85355712+CguarinoNJIT@users.noreply.github.com>
2021-07-01 20:13:37 -0400

    Update CalculatorTests.py
    added type hinting

--- Projects/P1/src/CalculatorTests.py
+++ Projects/P1/src/CalculatorTests.py
@@ -10,49 +10,49 @@ class MyTestCase(unittest.TestCase):
         self.calculator = Calculator()
 
 # This test_instantiate_calculator confirms that a Calculator Class exists.
-    def test_instantiate_calculator(self):
+    def test_instantiate_calculator(self) -> None:
         self.assertIsInstance(self.calculator, Calculator)
 
-    def test_results_property_calculator(self):
+    def test_results_property_calculator(self) -> None:
         self.assertEqual(self.calculator.result, 0)
 
 # Addition Check
-    def test_add_method_calculator(self):
+    def test_add_method_calculator(self) -> None:
         print("Addition: ")
         test_data = CsvReader('/src/Unit Test Addition.csv').data
         for row in test_data:
             self.assertEqual(self.calculator.add(int(row['Value 1']),int(row['Value 2'])), int(row['Result']))
             self.assertEqual(self.calculator.result, int(row['Result']))
 # Subtraction Check
-    def test_subtract_method_calculator(self):
+    def test_subtract_method_calculator(self) -> None:
         print("Subtraction: ")
         test_data = CsvReader('/src/Unit Test Subtraction.csv').data
         for row in test_data:
             self.assertEqual(self.calculator.subtract(int(row['Value 1']),int(row['Value 2'])), int(row['Result']))
             self.assertEqual(self.calculator.result, int(row['Result']))
 # Multiplication Check
-    def test_multiply_method_calculator(self):
+    def test_multiply_method_calculator(self) -> None:
         print("Multiplication: ")
         test_data = CsvReader('/src/Unit Test Multiplication.csv').data
         for row in test_data:
             self.assertEqual(self.calculator.multiply(int(row['Value 1']),int(row['Value 2'])), int(row['Result']))
             self.assertEqual(self.calculator.result, int(row['Result']))
 # Division Check
-    def test_division_method_calculator(self):
+    def test_division_method_calculator(self) -> None:
         print("Division: ")
         test_data = CsvReader('/src/Unit Test Division.csv').data
         for row in test_data:
             self.assertEqual(self.calculator.divide(int(row['Value 1']),int(row['Value 2'])), int(row['Result']))
             self.assertEqual(self.calculator.result, int(row['Result']))
 # Square Root Check
-    def test_squareroot_method_calculator(self):
+    def test_squareroot_method_calculator(self) -> None:
         print("Square Root: ")
         test_data = CsvReader('/src/Unit Test Square Root.csv').data
         for row in test_data:
             self.assertEqual(self.calculator.square_root(int(row['Value 1'])), int(row['Result']))
             self.assertEqual(self.calculator.result, int(row['Result']))
 # Square Check
-    def test_squared_method_calculator(self):
+    def test_squared_method_calculator(self) -> None:
         print("Squared: ")
         test_data = CsvReader('/src/Unit Test Square.csv').data
         for row in test_data:
