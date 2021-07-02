c7bd45ffe47c945dee5d427d7540c33d72c2c8aa
CguarinoNJIT <85355712+CguarinoNJIT@users.noreply.github.com>
2021-07-01 20:17:31 -0400

    Update CsvReader.py

--- Projects/P1/src/CsvReader.py
+++ Projects/P1/src/CsvReader.py
@@ -1,13 +1,14 @@
 import csv
 from pprint import pprint
+from typing import Dict
 
-def ClassFactory(class_name, dictionary):
+def ClassFactory(class_name: str, dictionary: Dict[str, object]) -> object:
     return type(class_name,(object,),dictionary)
 
 class CsvReader:
     data = []
 
-    def __init__(self,filepath):
+    def __init__(self,filepath: str) -> None:
         with open(filepath) as text_data:
             csv_data = csv.DictReader(text_data,delimiter = ',')
             for row in csv_data:
@@ -15,7 +16,7 @@ class CsvReader:
             pprint(self.data)
         pass
 
-    def return_data_as_objects(self,class_name):
+    def return_data_as_objects(self,class_name: str) -> object:
         objects = []
         for row in self.data:
             objects.append(ClassFactory(class_name,row))
