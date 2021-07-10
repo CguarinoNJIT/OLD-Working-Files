58af577c0fc8670c7045eb877b2f487c4e795f92
Chris Guarino <chris.francis.guarino@gmail.com>
2021-07-10 15:53:22 -0400

    Task: Completed Median Module

--- Projects/P2/src/Statistics_Calculator/Median.py
+++ Projects/P2/src/Statistics_Calculator/Median.py
@@ -1,19 +1,22 @@
-from Calculator import Division,Addition
-from Support import Random_List_Generator
+from Calculator.Addition import addition
+from Calculator.Subtraction import subtraction
+from Calculator.Division import division
+from Support.Random_List_Generator import random_list_generator
 
 def median(sample):
     sample.sort()
     length = len(sample)
     print(f'List Length: {length}')
-    centernum = int(Division.division(2,length))
+    centernum = int(division(2,length))
     if length % 2 == 0:
-        evencenternum = Division.division(2,Addition.addition(sample[centernum] + sample[Addition.addition(centernum,1)]))
-        return sample[evencenternum]
+        evencenternum = int(division(2,addition(sample[centernum] , sample[subtraction(1,centernum)])))
+        print(sample[centernum],sample[centernum-1])
+        return evencenternum
     else:
         return sample[centernum]
 
 
-testlist = Random_List_Generator.random_list_generator(10,1)
+testlist = random_list_generator(10)
 print(f'List: {testlist}')
 
 print(median(testlist))
